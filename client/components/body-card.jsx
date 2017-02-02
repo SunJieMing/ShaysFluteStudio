3a412be90c7cc1ef307a69f1904b214ed894b5d8
Ben Nelson <benn@ifit.com>
2017-02-02 15:19:26 -0700

    Updated font-weight and body styles

--- client/components/body-card.jsx
+++ client/components/body-card.jsx
@@ -3,6 +3,7 @@ import styles from '../styles/body-styles.css';
 
 const BodyCard = () => (
 	<li className={styles.bodyCard}>
+		<h2 className={styles.bodyCardHeader}>Test Header</h2>
 		<img className={styles.bodyCardImage} src='https://images.pexels.com/photos/164811/pexels-photo-164811.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb'/>
 		<p className={styles.bodyCardCopy}>Test Test Test Test Test Test Test Test Test Test Test Test Test Test Test </p>
 	</li>
